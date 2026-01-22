0376f34c678f856a4c62b7512675410b35d3759c
PainKiller3 <ninadpatil100@gmail.com>
2026-01-22 14:14:35 +0530

    feat: add PWA support and update dependencies
    - Added vite-plugin-pwa to package.json for Progressive Web App support.
    - Fixed the path in site.webmanifest for the Android icon.
    - Updated main.jsx to use createRoot from React 18 and register the service worker automatically.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // automatically updates SW
+      manifest: {
+        name: "Workout Tracker",
+        short_name: "Workout Tracker",
+        description: "Workout Tracker App",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/android/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+          {
+            src: "/android/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable any",
+          },
+        ],
+      },
+    }),
+  ],
+});
