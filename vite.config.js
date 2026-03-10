20d7fce286b0b741d2fa90df224d24fe14b79e59
PainKiller3 <ninadpatil100@gmail.com>
2026-03-10 22:12:25 +0530

    chore(release): bump version 1.5.0

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const APP_VERSION = "1.4.0";
+const APP_VERSION = "1.5.0";
 
 // https://vite.dev/config/
 export default defineConfig({
